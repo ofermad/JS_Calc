84d4e2234b1d57c71598b14c3dbc38767cf3183b
ofermad <redturner@mail.ru>
2024-01-11 15:43:42 +0300

    First commit

---
+++ for_test.js
@@ -0,0 +1,12 @@
+//alert("Calculator");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+document.body.addEventListener('click', function(e){
+    var b = e.target;
+    if(b.tagName == 'BUTTON'){
+        b.innerHTML;
+    }
+}, false);
+
+});
